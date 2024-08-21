2acffba522f7849517c204d68b11f9b00ef11cf1
Jorge Gomez <43116542+caXos@users.noreply.github.com>
2024-08-21 11:08:53 -0300

    Update spotifyApis.js
    Changed redirect_uri from localhost to the deployed version

--- src/methods/spotifyApis.js
+++ src/methods/spotifyApis.js
@@ -1,7 +1,8 @@
 import { toast } from "react-toastify";
 const stateKey = "spotify_auth_state";
 const client_id = "34643a9bb95144ee936c43a5cf863256";
-const redirect_uri = "http://localhost:3000/";
+// const redirect_uri = "http://localhost:3000/";
+const redirect_uri = "https://jammming-brown.vercel.app/";
 const scope = "playlist-modify-public";
 
 /**
